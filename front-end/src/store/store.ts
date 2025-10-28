f6c92eeba82ac3ba7377c0b7e0b1950fa41d93fb
jimay <jimy.xu@epitech.eu>
2025-10-28 21:23:25 +0100

    feat: persisting store

--- front-end/src/store/store.ts
+++ front-end/src/store/store.ts
@@ -1,13 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import cartReducer from './cart';
 import sidebarReducer from './sidebar';
 
-export const store = configureStore({
-	reducer: {
-		cart: cartReducer,
-		sidebar: sidebarReducer,
-	},
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+	cart: cartReducer,
+	sidebar: sidebarReducer,
 });
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: ['cart'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+	reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
